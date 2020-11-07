53c22aab966b48d329c0a2760c7b4de1d5292588
Z3R0ing <lytin.daniil@gmail.com>
2020-11-07 14:21:59 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
-import '@vkontakte/vkui/dist/vkui.css';
 import Root from '@vkontakte/vkui/dist/components/Root/Root';
-
 import TestView from './view/TestView';
-import MainView from './view/MainView';
-import Edit from './view/Edit';
 import MoreInfo from './view/MoreInfo';
+/*import MainView from './view/MainView';
+import Edit from './view/Edit';
 import Share from './view/Share';
-import ShareMore from './view/ShareMore';
+import ShareMore from './view/ShareMore';*/
+import '@vkontakte/vkui/dist/vkui.css';
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('test');
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+	const [activeView] = useState('test');
 
 	return (
-		<Root activeView={this.state.activeView}>
-			<TestView id='test' go={go}/>
-			<MainView id='main' go={go}/>
-			<Edit id='edit' go={go}/>
-			<MoreInfo id='moreInfo' go={go}/>
-			<Share id='share' go={go}/>
-			<ShareMore id='shareMore' go={go}/>
+		<Root activeView={activeView}>
+			<TestView id = 'test'/>
+			<MoreInfo id = 'moreInfo'/>
 		</Root>
 	);
 }
--- src/view/TestView.js
+++ src/view/TestView.js
@@ -1,50 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import View from '@vkontakte/vkui/dist/components/View/View';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import CellButton from '@vkontakte/vkui/dist/components/CellButton/CellButton';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
 
-class TestViev extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        activePanel: 'panel1'
-      }
-    }
-  
-    render() {
-      return (
-        <View activePanel={this.state.activePanel}>
-          <Panel id="panel1">
-            <PanelHeader>Panel 1</PanelHeader>
-            <Group>
-              <CellButton onClick={ () => this.setState({ activePanel: 'panel2' }) }>
-                Go to panel 2
-              </CellButton>
-            </Group>
-          </Panel>
-          <Panel id="panel2">
-            <PanelHeader>Panel 2</PanelHeader>
-            <Group>
-              <CellButton onClick={ () => this.setState({ activePanel: 'panel3' }) }>
-                Go to panel 3
-              </CellButton>
-            </Group>
-          </Panel>
-          <Panel id="panel3">
-            <PanelHeader>Panel 3</PanelHeader>
-            <Group>
-              <CellButton onClick={ () => this.setState({ activePanel: 'panel1' }) }>
-                Back to panel 1
-              </CellButton>
-            </Group>
-          </Panel>
-        </View>
-      )
-    }
-  }
-  export default TestViev;
+const TestView = ({ id }) => (
+    <View id = { id } activePanel='test1'>
+	<Panel id="test1">
+		<PanelHeader>Example</PanelHeader>
+        <Div>
+            <Button>
+				Кнопка
+			</Button>
+        </Div>
+	</Panel>
+    </View>
+);
+
+TestView.propTypes = {
+    id: PropTypes.string.isRequired
+};
+
+export default TestView;
