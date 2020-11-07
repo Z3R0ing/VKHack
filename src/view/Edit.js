0311cf43a965ab5f41650ce65ffbe6f8f51f47c3
Halacky <RomeFellSoSad@gmail.com>
2020-11-07 15:18:40 +0800

    Update edit
    Update edit

--- src/view/Edit.js
+++ src/view/Edit.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { platform, IOS } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
-import Input from 'react';
-import FormLayoutGroup from 'react';
+import Input from '@vkontakte/vkui/dist/components/Input/Input';
+import FormLayoutGroup from '@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup';
 
-const osName = platform();
-
-const Edit = props => (
-	<Panel id={props.id}>
+const Edit = ({ id , go }) => (
+	<Panel id={id}>
 		<PanelHeader
-			left={<PanelHeaderButton onClick={props.go} data-to="test">
-			</PanelHeaderButton>}
+		left={<PanelHeaderButton onClick={ go } data-on='test'>Назад</PanelHeaderButton>}
 		>
 			Edit Frame
 		</PanelHeader>
-
         <FormLayoutGroup top="Работа">
               <Input placeholder="Место работы" />
               <Input placeholder="Должность" />
-            </FormLayoutGroup>
+        </FormLayoutGroup>
 	</Panel>
-
-    
 );
 
 Edit.propTypes = {
