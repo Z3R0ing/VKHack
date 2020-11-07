ad953b30ffbbe43e9425b015d298068751cfe5b8
Z3R0ing <lytin.daniil@gmail.com>
2020-11-07 21:53:05 +0800

    Add Main screen
    Co-Authored-By: Halacky <55797478+Halacky@users.noreply.github.com>

--- src/panels/MainView.js
+++ src/panels/MainView.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
-import View from '@vkontakte/vkui/dist/components/View/View';
+import { View, Button, CardGrid, Checkbox, Div, FormLayout } from '@vkontakte/vkui';
+import DatePicker from 'react-date-picker';
+import Calendar from 'react-calendar';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Accordion, Card} from 'react-bootstrap';
 
-const MainView = ({ id , go }) => (
-<View id={ id } activePanel = 'mainPanel'>
+const MainView = ({ id , go }) => {
+		const [date, setDate] = useState(new Date());
+		
+		const onChange =()=>{
+			setDate(date)
+		}
+
+		return(
+			<View id={ id } activePanel = 'mainPanel'>
 	<Panel id='mainPanel'>
-		<PanelHeader left={<PanelHeaderButton onClick={ go } data-to='test'>{'Назад'}</PanelHeaderButton>}>
-			StudORG
-		</PanelHeader>
-	</Panel>
-</View>
-);
+			<PanelHeader left={<PanelHeaderButton onClick={ go } data-to='test'>{'Назад'}</PanelHeaderButton>}>
+				StudORG
+			</PanelHeader>
+			<Div>
+			<Calendar onChange={onChange} value={date} />
+			</Div>
+			
+			<Accordion defaultActiveKey="0">
+  				<Card>
+    				<Accordion.Toggle as={Card.Header} eventKey="0">Инженерная графика </Accordion.Toggle>
+    				<Accordion.Collapse eventKey="0">
+					<CardGrid>	
+						<FormLayout>
+        					<Checkbox>Подзадача 1</Checkbox>
+        					<Checkbox>Подзадача 2</Checkbox>
+        					<Checkbox>Подзадача 3</Checkbox>
+        					<Checkbox>Подзадача 4</Checkbox>
+      					</FormLayout>
+        			</CardGrid>
+    				</Accordion.Collapse>
+  				</Card>
+
+				  <Card>
+    				<Accordion.Toggle as={Card.Header} eventKey="1">Программирование</Accordion.Toggle>
+    				<Accordion.Collapse eventKey="1">
+					<CardGrid>	
+						<FormLayout>
+        					<Checkbox>Подзадача 1</Checkbox>
+        					<Checkbox>Подзадача 2</Checkbox>
+        					<Checkbox>Подзадача 3</Checkbox>
+        					<Checkbox>Подзадача 4</Checkbox>
+      					</FormLayout>
+        			</CardGrid>
+    				</Accordion.Collapse>
+  				</Card>
+			</Accordion>
+
+			</Panel>
+			</View>
+		);
+	
+};
 
 MainView.propTypes = {
 	id: PropTypes.string.isRequired,
