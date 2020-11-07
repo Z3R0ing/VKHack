e36e308a52b57e7f0434a6e5a1319915bdfd048e
Z3R0ing <lytin.daniil@gmail.com>
2020-11-07 17:40:42 +0800

    Update MoreInfo.js

--- src/panels/MoreInfo.js
+++ src/panels/MoreInfo.js
@@ -3,12 +3,59 @@ import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow';
+import { Cell } from '@vkontakte/vkui';
+import { FormLayout } from '@vkontakte/vkui';
+import { Checkbox } from '@vkontakte/vkui';
+import { Link } from '@vkontakte/vkui';
+import { Header } from '@vkontakte/vkui';
 
 const MoreInfo = ({ id , go }) => (
 	<Panel id={id}>
 		<PanelHeader left={<PanelHeaderButton onClick={ go } data-to='test'>{'Назад'}</PanelHeaderButton>}>
-			Подробнее
+			Программирование
 		</PanelHeader>
+      <Group>
+        <Cell>
+          <InfoRow header="Тип задания">
+            Лабораторная
+          </InfoRow>
+        </Cell>
+        <Cell>
+          <InfoRow header="Срок для задания">
+            8 ноября 2020
+          </InfoRow>
+        </Cell>
+		<Cell multiline>
+			<InfoRow header="Подзадачи">
+				<FormLayout>
+					<Checkbox>Установить IDE</Checkbox>
+					<Checkbox>Настроить IDE</Checkbox>
+					<Checkbox>Программа</Checkbox>
+					<Checkbox>Отчёт</Checkbox>
+				</FormLayout>
+			</InfoRow>
+		</Cell>
+        <Cell multiline>
+          <InfoRow header="Заметки">
+            Лабу делать в NewIDE. <br></br>
+			Для старта: <br></br>
+			`trasher install <br></br>
+			`trasher start <br></br>
+          </InfoRow>
+        </Cell>
+        <Cell multiline>
+          <InfoRow header="Прикреплённые файлы">
+            ХЗ ЧО ТУТ ДЕЛАТЬ
+          </InfoRow>
+        </Cell>
+        <Cell>
+          <InfoRow header="Преподаватель">
+            Дорофеев А.С.
+          </InfoRow>
+        </Cell>
+      </Group>
 	</Panel>
 );
 
