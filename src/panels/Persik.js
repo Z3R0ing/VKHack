5fe7f4632e5e64f711c270c2ab989686b652f2a2
Halacky <RomeFellSoSad@gmail.com>
2020-11-07 12:15:34 +0800

    Update Persik
    Undo update Persik

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -6,17 +6,17 @@ import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
+
 import persik from '../img/persik.png';
 import './Persik.css';
+
+
 const osName = platform();
 
-const Persik = (id, go) => (
-	<Panel id={id}>
+const Persik = props => (
+	<Panel id={props.id}>
 		<PanelHeader
-			left={<PanelHeaderButton onClick={go} data-to="home">
+			left={<PanelHeaderButton onClick={props.go} data-to="home">
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</PanelHeaderButton>}
 		>
@@ -24,13 +24,7 @@ const Persik = (id, go) => (
 		</PanelHeader>
 		<img className="Persik" src={persik} alt="Persik The Cat"/>
 
-		<Group title="Main Frame">
-			<Div>
-				<Button size="xl" level="3" onClick={go} data-to="main">
-					Show me the Main, please
-				</Button>
-			</Div>
-		</Group>
+		
 
 	</Panel>
 );
