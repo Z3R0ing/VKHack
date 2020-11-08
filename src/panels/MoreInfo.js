a008932ec413959379c925fc50c95aebabbeee52
Z3R0ing <lytin.daniil@gmail.com>
2020-11-08 09:20:50 +0800

    Cards in MoreInfo and Back Button

--- src/panels/MoreInfo.js
+++ src/panels/MoreInfo.js
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow';
 import { Cell } from '@vkontakte/vkui';
 import { FormLayout } from '@vkontakte/vkui';
 import { Checkbox } from '@vkontakte/vkui';
-import { Link } from '@vkontakte/vkui';
-import { Header } from '@vkontakte/vkui';
 import { View } from '@vkontakte/vkui';
-import{PanelHeaderBack} from '@vkontakte/vkui';
+import{PanelHeaderBack, PanelHeaderContent, Card} from '@vkontakte/vkui';
+
+
 const MoreInfo = ({ id , go }) => (
 <View id={id} activePanel='infoPanel'>
 	<Panel id='infoPanel'>
-  <PanelHeader left={<PanelHeaderBack onClick={ go } data-to='test'>{'Назад'}</PanelHeaderBack>}></PanelHeader>
-    <h2 style={{textAlignVertical: "center",textAlign: "center"}}>Программирование</h2>	
+  <PanelHeader left = {<PanelHeaderBack onClick={ go } data-to='test'/>}><PanelHeaderContent>Программирование</PanelHeaderContent></PanelHeader>
+    
       <Group>
         <Cell>
           <InfoRow header="Тип задания">
@@ -30,12 +29,14 @@ const MoreInfo = ({ id , go }) => (
         </Cell>
 		<Cell multiline>
 			<InfoRow header="Подзадачи">
+      <Card size="l" mode="outline">
 				<FormLayout>
 					<Checkbox>Установить IDE</Checkbox>
 					<Checkbox>Настроить IDE</Checkbox>
 					<Checkbox>Программа</Checkbox>
 					<Checkbox>Отчёт</Checkbox>
 				</FormLayout>
+      </Card>
 			</InfoRow>
 		</Cell>
         <Cell multiline>
