488a963748001c764edaa2b97c88ad11113c3d72
Halacky <RomeFellSoSad@gmail.com>
2020-11-08 08:33:24 +0800

    Edit Share.js
    Added accor on Share frame

--- src/panels/Share.js
+++ src/panels/Share.js
@@ -4,14 +4,46 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import View from '@vkontakte/vkui/dist/components/View/View';
-import{PanelHeaderBack} from '@vkontakte/vkui';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Accordion} from 'react-bootstrap';
+import{Card,Header, Cell, Checkbox, FormLayout, PanelHeaderBack, Div} from '@vkontakte/vkui';
+
 
 const Share = ({ id , go }) => (
 <View id={id} activePanel='sharePanel'>
 	<Panel id='sharePanel'>
 	<PanelHeader left={<PanelHeaderBack onClick={ go } data-to='test'>{'Назад'}</PanelHeaderBack>}></PanelHeader>
 		<h2 style={{textAlignVertical: "center",textAlign: "center"}}>Поделились</h2>	
-		
+		<Accordion defaultActiveKey="-1">
+			<Div>
+  				<Card>
+    				<Accordion.Toggle as={Header} eventKey="0">Инженерная графика </Accordion.Toggle>
+    				<Accordion.Collapse eventKey="0">
+						<FormLayout>
+        					<Checkbox>Подзадача 1</Checkbox>
+        					<Checkbox>Подзадача 2</Checkbox>
+        					<Checkbox>Подзадача 3</Checkbox>
+        					<Checkbox>Подзадача 4</Checkbox>
+							<Cell>Cрок: 18.11</Cell>
+      					</FormLayout>
+    				</Accordion.Collapse>
+  				</Card>
+				  </Div>
+			<Div>
+				  <Card>
+				  <Accordion.Toggle as={Header} eventKey="1">Программирование</Accordion.Toggle>
+    				<Accordion.Collapse eventKey="1">
+						<FormLayout>
+        					<Checkbox>Подзадача 1</Checkbox>
+        					<Checkbox>Подзадача 2</Checkbox>
+        					<Checkbox>Подзадача 3</Checkbox>
+        					<Checkbox>Подзадача 4</Checkbox>
+							<Cell>Cрок: 18.11</Cell>
+      					</FormLayout>
+    				</Accordion.Collapse>
+  				</Card>
+			</Div>
+			</Accordion>
 	</Panel>
 </View>
 );
